347fdb8e3804a77f9ff357a10fedc4d9bb2b6d0b
Elissa Levy <elissadunnlevy@gmail.com>
2020-07-26 18:38:34 +0000

    db updates work, no logins yet

--- app.py
+++ app.py
@@ -1,46 +1,44 @@
 # ---- YOUR APP STARTS HERE ----
 # -- Import section --
-from flask import Flask
-from flask import render_template
-from flask import request
-# from flask_pymongo import PyMongo
+from flask import Flask, render_template, request
+from flask_pymongo import PyMongo
 
 
 # -- Initialization section --
 app = Flask(__name__)
 
-events = [
-        {"event":"First Day of Classes", "date":"2019-08-21"},
-        {"event":"Winter Break", "date":"2019-12-20"},
-        {"event":"Finals Begin", "date":"2019-12-01"}
-    ]
-
 # name of database
-# app.config['MONGO_DBNAME'] = 'database-name'
+app.config['MONGO_DBNAME'] = 'database'
 
 # URI of database
-# app.config['MONGO_URI'] = 'mongo-uri'
+app.config['MONGO_URI'] = 'mongodb+srv://admin:moninananM4!@cluster0-qezhz.mongodb.net/database?retryWrites=true&w=majority'
 
-# mongo = PyMongo(app)
+mongo = PyMongo(app)
 
 # -- Routes section --
 # INDEX
 
 @app.route('/')
 @app.route('/index')
-
 def index():
-    return render_template('index.html', events = events)
-
+    collection = mongo.db.events
+    events = collection.find({})
+    return render_template('index.html', events=events)
 
 # CONNECT TO DB, ADD DATA
 
-@app.route('/add')
+@app.route('/newevent', methods = ['GET', 'POST'])
+def new_event():
+    if request.method == 'GET':
+        return render_template('new_event.html')
+    else:
+        # collect data from the new_event.html form 
+        event_name = request.form['event_name']
+        event_date = request.form['event_date']
+        user_name = request.form['user_name']
 
-def add():
-    # connect to the database
-
-    # insert new data
-
-    # return a message to the user
-    return ""
+        # insert data into database
+        collection = mongo.db.events
+        collection.insert({"event":event_name, "date":event_date, "user": user_name})
+     
+        return "Data added.  Go to <a href='/index'>home</a>."
